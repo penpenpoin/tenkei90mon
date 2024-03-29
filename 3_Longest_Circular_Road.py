6cfbd20f92de835338edc8f08b289aa92c791ba7
penpenpoin <85094779+penpenpoin@users.noreply.github.com>
2024-03-29 13:07:54 +0900

    Update 3_Longest_Circular_Road.py

--- 3_Longest_Circular_Road.py
+++ 3_Longest_Circular_Road.py
@@ -1,42 +1,22 @@
 N = int(input())
-#つながっている街のリスト
-connect_city_list = [[]for i in range(N)]
-for i  in range(N-1):
-    a, b = map(int, input().split())
-    a -= 1
-    b -= 1
-    connect_city_list[int(a)].append(b)
-    connect_city_list[int(b)].append(a)
-#dfs
-ans = 0
-def dfs(start):
-    global ans
-    num = 0
-    branching_num =[0 for i in range(N-1)]
-    stack = [start]
-    seen = [start]
-    most_far = 1
-    while stack:
-        node = stack.pop()
-        way_of_num = 0
-        for i in connect_city_list[node]:
-            if not i in seen:
-                stack.append(i)
-                seen.append(i)
-                way_of_num += 1
-        if way_of_num > 1:
-            num +=1
-            branching_num.append(num)
-        if way_of_num == 1:
-            num +=1
-        elif way_of_num == 0:
-            tmp = ans
-            ans = max(num,ans)
-            if not tmp == ans:
-                most_far = node
-            num = branching_num.pop()
-    return most_far
-    
-ppp = dfs(0)
-dfs(ppp)
-print(ans+1)
+connection_city_list = [[] for i in range(N)]
+for _ in range(N-1):
+    a,b = map(int,input().split())
+    connection_city_list[a-1].append(b-1)
+    connection_city_list[b-1].append(a-1)
+
+def bfs(city_num):
+    que = [city_num]
+    seen = [0] * N
+    depth = [0]*N
+    depth[city_num] = 1
+    while que:
+        node = que.pop()
+        seen[node] = 1
+        for n in connection_city_list[node]:
+            if  seen[n] == 0:
+                que.append(n)
+                depth[n]= depth[node] + 1
+    return depth
+
+print(max(bfs(max(enumerate(bfs(0)),key=lambda x:x[1])[0])))
